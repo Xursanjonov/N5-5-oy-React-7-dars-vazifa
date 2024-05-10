027b465c13ad817d2d9e96633dbc6b4e36727b5f
Nusratillo <nxursanjonov888@gmail.com>
2024-05-10 22:44:04 +0500

    no product and Shop Premium

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,31 @@
 import React, { Fragment } from "react"
 import Header from "./components/layout/header"
 import Footer from "./components/layout/footer"
+import Haro from "./components/hero/Haro"
+import ThreeCards from "./components/threeCards/ThreeCards"
+import { threeProducts } from "./static"
+import ThreeImages from "./components/threeImages/ThreeImages"
+import SectionIcon from "./components/sections/SectionIcon"
 
 function App() {
+
+  const threeCard = threeProducts?.map((product) => <ThreeCards key={product.id} {...product} />)
 
   return (
     <Fragment>
       <Header />
       <main className="main container">
-        <h1>Main</h1>
+        <Haro />
+        <section className='SectionTitle'>
+          <SectionIcon name={'about us'} />
+          <h1 className="sectionTitle__text"> If you're looking for a Premium Quality Tempered Glass or Back-cover for your Device </h1>
+          <p className="sectionTitle__info">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+          </p>
+        </section>
+        <ThreeImages />
+        <SectionIcon name='Why Choose US' />
+        <section className="threeCards"> {threeCard} </section>
       </main>
       <Footer />
     </Fragment>
--- src/components/layout/header/index.jsx
+++ src/components/layout/header/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import Logo from '../../../assets/icons/Logo'
 import Input from '../../formComponents/Input'
 import Button from '../../formComponents/Button'
@@ -6,27 +6,28 @@ import { FaBars, FaSearch } from 'react-icons/fa'
 import './header.scss'
 
 const Header = () => {
-  const [showMenu, setShowMenu] = React.useState(false)
 
   return (
-    <header className='header'>
-      <nav className="header__nav">
-        <div className="header__nav__list">
-          <a href="#" className='header__nav__list-item'>Home</a>
-          <a href="#" className='header__nav__list-item'>Shop All</a>
-          <a href="#" className='header__nav__list-item'>Blog</a>
-        </div>
-        <Logo />
-        <div className="header__nav__search-div">
-          <a href="#" className='header__nav__search-div__item'>About Us</a>
-          <form className="header__nav__search-div__form">
-            <Input className={'header__nav__search-div__form__input'} type={'search'} placeholder={'Search Product'} />
-            <FaSearch />
-          </form>
-        </div>
-        <Button onClick={() => setShowMenu(true)} className="header__nav__bars"> <FaBars /> </Button>
-      </nav>
-    </header>
+    <Fragment>
+      <header className='header'>
+        <nav className="header__nav">
+          <div className="header__nav__list">
+            <a href="#" className='header__nav__list-item'>Home</a>
+            <a href="#" className='header__nav__list-item'>Shop All</a>
+            <a href="#" className='header__nav__list-item'>Blog</a>
+          </div>
+          <Logo />
+          <div className="header__nav__search-div">
+            <a href="#" className='header__nav__search-div__item'>About Us</a>
+            <form className="header__nav__search-div__form">
+              <Input className={'header__nav__search-div__form__input'} type={'search'} placeholder={'Search Product'} />
+              <FaSearch />
+            </form>
+          </div>
+          <Button onClick={() => setShowMenu(true)} className="header__nav__bars"> <FaBars /> </Button>
+        </nav>
+      </header>
+    </Fragment>
   )
 }
 
